00558d894842de07f34a2c38399af3455eb942ba
Josef Schulz <j.schulz@alaun.de>
2025-10-16 01:18:41 +0200

    update c version

--- efi.h
+++ efi.h
@@ -3,6 +3,14 @@
 
 #include <stdint.h>
 
+typedef void* EFI_HANDLE;
+typedef void* EFI_EVENT;
+
+typedef struct {
+	uint16_t ScanCode;
+	uint16_t UnicodeChar;
+} EFI_INPUT_KEY;
+
 typedef struct {
 	uint64_t Signature;  
 	uint32_t Revision;
@@ -14,7 +22,7 @@ typedef struct {
 typedef struct {
 	void* Reset;
 	void* ReadKeyStroke;
-	void* WaitForKey;
+	EFI_EVENT WaitForKey;
 } EFI_SIMPLE_TEXT_INPUT_PROTOCOL;
 
 typedef struct {
@@ -32,20 +40,89 @@ typedef struct {
 
 typedef struct {
 	EFI_TABLE_HEADER Hdr;
+	void* RaiseTPL;
+	void* RestoreTPL;
+	void* AllocatePages;
+	void* FreePages;
+	void* GetMemoryMap;
+	void* AllocatePool;
+	void* FreePool;
+	void* CreateEvent;
+	void* SetTimer;
+	void* WaitForEvent;
+	void* SignalEvent;
+	void* CloseEvent;
+	void* CheckEvent;
+	void* InstallProtocolInterface;
+	void* ReinstallProtocolInterface;
+	void* UninstallProtocolInterface;
+	void* HandleProtocol;
+	void* Reserved;
+	void* RegisterProtocolNotify;
+	void* LocateHandle;
+	void* LocateDevicePath;
+	void* InstallConfigurationTable;
+	void* LoadImage;
+	void* StartImage;
+	void* Exit;
+	void* UnloadImage;
+	void* ExitBootServices;
+	void* GetNextMonotonicCount;
+	void* Stall;
+	void* SetWatchdogTimer;
+	void* ConnectController;
+	void* DisconnectController;
+	void* OpenProtocol;
+	void* CloseProtocol;
+	void* OpenProtocolInformation;
+	void* ProtocolsPerHandle;
+	void* LocateHandleBuffer;
+	void* LocateProtocol;
+	void* InstallMultipleProtocolInterfaces;
+	void* UninstallMultipleProtocolInterfaces;
+	void* CalculateCrc32;
+	void* CopyMem;
+	void* SetMem;
+	void* CreateEventEx;
+} EFI_BOOT_SERVICES;
+
+typedef struct {
+	EFI_TABLE_HEADER Hdr;
+	void* GetTime;
+	void* SetTime;
+	void* GetWakeupTime;
+	void* SetWakeupTime;
+	void* SetVirtualAddressMap;
+	void* ConvertPointer;
+	void* GetVariable;
+	void* GetNextVariableName;
+	void* SetVariable;
+	void* GetNextHighMonotonicCount;
+	void* ResetSystem;
+	void* UpdateCapsule;
+	void* QueryCapsuleCapabilities;
+	void* QueryVariableInfo;
+} EFI_RUNTIME_SERVICES;
+
+typedef struct {
+	EFI_TABLE_HEADER Hdr;
 	void* FirmwareVendor;
 	uint32_t FirmwareRevision;
-	void* ConsoleInHandle;
+	EFI_HANDLE ConsoleInHandle;
 	EFI_SIMPLE_TEXT_INPUT_PROTOCOL* ConIn;
-	void* ConsoleOutHandle;
+	EFI_HANDLE ConsoleOutHandle;
 	EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* ConOut;
-	void* StandardErrorHandle;
+	EFI_HANDLE StandardErrorHandle;
 	void* StdErr;
-	void* RuntimeServices;
-	void* BootServices;
+	EFI_RUNTIME_SERVICES* RuntimeServices;
+	EFI_BOOT_SERVICES* BootServices;
 	uint64_t NumberOfTableEntries;
 	void* ConfigurationTable;
 } EFI_SYSTEM_TABLE;
 
 typedef void (*EFI_TEXT_OUTPUT_STRING) (EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* ConOut, uint16_t* String);
+typedef void (*EFI_WAIT_FOR_EVENT) (uint64_t NumberOfEvents, EFI_EVENT* Event, uint64_t* Index);
+typedef void (*EFI_STALL) (uint64_t Microseconds);
+typedef void (*EFI_INPUT_READ_KEY) (EFI_SIMPLE_TEXT_INPUT_PROTOCOL *This, EFI_INPUT_KEY *Key);
 
 #endif
--- main.c
+++ main.c
@@ -1,11 +1,21 @@
 #include "efi.h"
+#include <stdint.h>
 
 int _start(void* image, EFI_SYSTEM_TABLE* systemTable) {
 	EFI_TEXT_OUTPUT_STRING OutputString = (EFI_TEXT_OUTPUT_STRING)systemTable->ConOut->OutputString;
-	uint16_t strHello[] = u"\rHallo World";
-	OutputString(systemTable->ConOut, strHello);
-	for(;;) {
+	EFI_WAIT_FOR_EVENT WaitForEvent = (EFI_WAIT_FOR_EVENT)systemTable->BootServices->WaitForEvent;
+	EFI_STALL Stall = (EFI_STALL)systemTable->BootServices->Stall;
+	EFI_INPUT_READ_KEY readKeyStroke = (EFI_INPUT_READ_KEY)systemTable->ConIn->ReadKeyStroke;
+
+	uint16_t strHello[] = u"Hallo World\n\r\0";
+	uint64_t index;
+	EFI_INPUT_KEY key;
+
+	for(int i = 0; i < 9; i++) {
 		OutputString(systemTable->ConOut, strHello);
+		WaitForEvent(1, &systemTable->ConIn->WaitForKey, &index);
+		readKeyStroke(systemTable->ConIn, &key);
 	}
+
     return 0;
 }
